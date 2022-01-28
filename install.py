62118ffd994e4e0b4f87a012a993465c04a87fef
amagiria <96608658+amagiria@users.noreply.github.com>
2022-01-28 23:46:44 +1000

    Update install.py

--- install.py
+++ install.py
@@ -4,15 +4,15 @@ run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
     run('chmod 777 autoTOR.py')
-    run('mkdir /usr/share/ipp')
-    run('cp autoTOR.py /usr/share/ipp/autoTOR.py')
+    run('mkdir /usr/share/rr')
+    run('cp autoTOR.py /usr/share/rr/autoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/ipp/autoTOR.py "$@"')
-    with open('/usr/bin/ipp','w')as file:
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/rr/autoTOR.py "$@"')
+    with open('/usr/bin/rr','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/ipp & chmod +x /usr/share/ipp/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42mipp\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/rr & chmod +x /usr/share/rr/autoTOR.py')
+    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42mrr\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
-    run('rm -r /usr/share/ipp ')
-    run('rm /usr/bin/ipp ')
+    run('rm -r /usr/share/rr ')
+    run('rm /usr/bin/rr ')
     print('[!] now Auto Tor Ip changer  has been removed successfully')
