4e77c3a5470215ce2d6799e796cd24f15520b2ec
amagiria <96608658+amagiria@users.noreply.github.com>
2022-01-28 22:41:34 +1000

    Update autoTOR.py

--- autoTOR.py
+++ autoTOR.py
@@ -1,3 +1,40 @@
+#from PIL import Image
+from base64 import b64decode
+#from hashlib import sha1
+from io import BytesIO
+import names
+from hashlib import sha1
+import names
+import random
+import hmac
+import platform,socket,re,uuid 
+import base64
+import hmac
+import time
+import json
+from hashlib import sha1
+import secmail
+import random
+import platform,socket,re,uuid
+import json
+import time
+import os
+import subprocess
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+from time import time as timestamp
+import json
+from os import path
+#import amino
+#from colored import fg, bg, attr
+
+#from torpy.http.requests import TorRequests
+from hashlib import sha1
+import names
+import random
+import hmac
+import platform,socket,re,uuid 
 # -*- coding: utf-8 -*-
 
 import time
@@ -47,17 +84,6 @@ def change():
     os.system("service tor reload")
     print ('[+] Your IP has been Changed to : '+str(ma_ip()))
 
-print('''\033[1;32;40m \n
-                _          _______
-     /\        | |        |__   __|
-    /  \  _   _| |_ ___      | | ___  _ __
-   / /\ \| | | | __/ _ \     | |/ _ \| '__|
-  / ____ \ |_| | || (_) |    | | (_) | |
- /_/    \_\__,_|\__\___/     |_|\___/|_|
-                V 2.1
-from mrFD
-''')
-print("\033[1;40;31m http://facebook.com/ninja.hackerz.kurdish/\n")
 
 os.system("service tor start")
 
@@ -67,20 +93,121 @@ os.system("service tor start")
 time.sleep(3)
 print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
 os.system("service tor start")
-x = input("[+] time to change Ip in Sec [type=60] >> ")
-lin = input("[+] how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
-if int(lin) ==int(0):
+def dev():
+    hw=(names.get_full_name()+str(random.randint(0,10000000))+platform.version()+platform.machine()+names.get_first_name()+socket.gethostbyname(socket.gethostname())+':'.join(re.findall('..', '%012x' % uuid.getnode()))+platform.processor())
+    identifier=sha1(hw.encode('utf-8')).digest()
+    mac = hmac.new(bytes.fromhex('76b4a156aaccade137b8b1e77b435a81971fbd3e'), b"\x32" + identifier, sha1)
+    return (f"32{identifier.hex()}{mac.hexdigest()}").upper()
+ 
+def sig(data):
+        #at=json.dumps(data)
+        key='fbf98eb3a07a9042ee5593b10ce9f3286a69d4e2'
+        mac = hmac.new(bytes.fromhex(key), data.encode("utf-8"), sha1)
+        digest = bytes.fromhex("32") + mac.digest()
+        return base64.b64encode(digest).decode("utf-8")
+ 
+ 
+ 
+ 
+ 
+ 
+def gen_email():
+    mail = secmail.SecMail()
+    email = mail.generate_email()
+    return email
+    
+def upload(url):
+    link = requests.get(url)
+    result = BytesIO(link.content)
+    return result
+ 
+def get_message(email):
+                try:
+                    sleep(4)
+                    f=email
+                    mail = secmail.SecMail()
+                    inbox = mail.get_messages(f)
+                    for Id in inbox.id:
+                        msg = mail.read_message(email=f, id=Id).htmlBody
+                        bs = BeautifulSoup(msg, 'html.parser')
+                        images = bs.find_all('a')[0]
+                        url = (images['href']+'\n')
+                        if url is not None:
+                         print(url)
+                         return url
+                         #wget.download(url=url,out="code.png")
+                except:
+                    pass
+            
+ 
+         
+def register(nickname: str, email: str, password: str,deviceId: str):
+        data = {
+            "secret": f"0 {password}",
+            "deviceID": deviceId,
+            "email": email,
+            "clientType": 100,
+            "nickname": nickname,
+            "latitude": 0,
+            "longitude": 0,
+            "address": None,
+            "clientCallbackURL": "narviiapp://relogin",
+            "type": 1,
+            "identity": email,
+            "timestamp": int(timestamp() * 1000)
+        }
+        heads={
+    'Accept-Language': 'en-US', 
+    'Content-Type': 'application/json; charset=utf-8', 
+    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 7.1; LG-UK495 Build/MRA58K; com.narvii.amino.master/3.3.33180)', 
+    'Host': 'service.narvii.com', 
+    'Accept-Encoding': 'gzip',
+    'Connection': 'Keep-Alive',
+    }
+        data=json.dumps(data)
+        heads["NDC-MSG-SIG"]=sig(data)
+        heads["Content-Length"] = str(len(data))
+        heads["NDCDEVICEID"]=deviceId
+        response = requests.post(f"https://service.narvii.com/api/v1/g/s/auth/register", data=data, headers=heads)
+        print(response.text)   
+        
+def request_verify_code(email: str,deviceId: str):
+        data = {
+            "identity": email,
+            "type": 1,
+            "deviceID": deviceId
+        }
+        heads={
+    'Accept-Language': 'en-US', 
+    'Content-Type': 'application/json; charset=utf-8', 
+    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 7.1; LG-UK495 Build/MRA58K; com.narvii.amino.master/3.3.33180)', 
+    'Host': 'service.narvii.com', 
+    'Accept-Encoding': 'gzip',
+    'Connection': 'Keep-Alive',
+        }
+        data = json.dumps(data)
+        heads["Content-Length"] = str(len(data))
+        heads["NDCDEVICEID"]=deviceId
+        heads["NDC-MSG-SIG"]=sig(data)
+        response = requests.post(f"https://service.narvii.com/api/v1/g/s/auth/request-security-validation", data=data, headers=heads)
+ 
+ 
+ 
 
-	while True:
-		try:
-			time.sleep(int(x))
-			change()
-		except KeyboardInterrupt:
+ 
+ 
+def threadit():
+    deviceid=dev()
+    values=gen_email()
+    email=values
+    nick=names.get_first_name()
+    nick = "t.me/piececoin"
+    req=request_verify_code(email=email, deviceId=deviceid)
+    #vcode=verify(values)
+    register(nickname=nick, email=email, password="dfghjhdfg",deviceId=deviceid)
+ 
 
-		 	print('\nauto tor is closed ')
-		 	quit()
-
-else:
-	for i in range(int(lin)):
-		    time.sleep(int(x))
-		    change()
+while True:
+    for _ in range (3):
+        threadit()
+        change()
