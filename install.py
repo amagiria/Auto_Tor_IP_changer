b6c192a6ea42a98516f661559763aae64c0ba0f0
amagiria <96608658+amagiria@users.noreply.github.com>
2022-01-28 22:40:46 +1000

    Update install.py

--- install.py
+++ install.py
@@ -4,15 +4,15 @@ run = os.system
 if str(choice) =='Y' or str(choice)=='y':
 
     run('chmod 777 autoTOR.py')
-    run('mkdir /usr/share/aut')
-    run('cp autoTOR.py /usr/share/aut/autoTOR.py')
+    run('mkdir /usr/share/accgen')
+    run('cp autoTOR.py /usr/share/accgen/autoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/autoTOR.py "$@"')
-    with open('/usr/bin/aut','w')as file:
+    cmnd=(' #! /bin/sh \n exec python3 /usr/share/accgen/autoTOR.py "$@"')
+    with open('/usr/bin/accgen','w')as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
+    run('chmod +x /usr/bin/accgen & chmod +x /usr/share/accgen/autoTOR.py')
+    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maccgen\x1b[0m in terminal ''')
 if str(choice)=='N' or str(choice)=='n':
-    run('rm -r /usr/share/aut ')
-    run('rm /usr/bin/aut ')
+    run('rm -r /usr/share/accgen ')
+    run('rm /usr/bin/accgen ')
     print('[!] now Auto Tor Ip changer  has been removed successfully')
